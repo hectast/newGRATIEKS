e1bd95035b9618e356f028cc4126fc61526160d2
wayosu <wahyusman10@gmail.com>
2021-03-13 02:05:08 +0800

    include chartjs to js and modified navbar menu

--- views/layout/navbar.php
+++ views/layout/navbar.php
@@ -17,10 +17,10 @@
                       <a href="index3.html" class="nav-link nvlnk">Dashboard</a>
                   </li>
                   <li class="nav-item">
-                      <a href="index3.html" class="nav-link nvlnk">Dashboard</a>
+                      <a href="index3.html" class="nav-link nvlnk">Data Gratieks</a>
                   </li>
                   <li class="nav-item dropdown">
-                    <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link nvlnk dropdown-toggle">Dropdown</a>
+                    <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link nvlnk dropdown-toggle">Monitoring</a>
                     <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
                         <li><a href="#" class="dropdown-item">Some action </a></li>
                         <li><a href="#" class="dropdown-item">Some other action</a></li>
