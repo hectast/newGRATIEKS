2ae22e9d794385a278a38b5e6ae7e28fa5c10353
wayosu <wahyusman10@gmail.com>
2021-03-14 22:42:36 +0800

    pembaruan bug pada select2

--- views/layout/js.php
+++ views/layout/js.php
@@ -27,7 +27,9 @@
         });
 
         //Initialize Select2 Elements
-        $('.select2').select2();
+        $('.select2').select2({
+            theme: 'bootstrap4'
+        });
         //Initialize Select2 Elements
         $('.select2bs4').select2({
             theme: 'bootstrap4'
