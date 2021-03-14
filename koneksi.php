a8aecde818d9f58caa35b2502f6136a0459ed561
wayosu <wahyusman10@gmail.com>
2021-03-15 00:14:32 +0800

    pembaruan koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,10 +1,6 @@
 <?php
-define("HOST", "localhost");
-define("USER", "root");
-define("PASS", "");
-define("DBNAME", "phpdasar");
 
-$mysqli = new mysqli(HOST, USER, PASS, DBNAME);
+$mysqli = new mysqli("localhost", "root", "", "phpdasar");
 
 if ($mysqli->connect_error) {
     die("Koneksi Gagal: " . $mysqli->connect_error);
