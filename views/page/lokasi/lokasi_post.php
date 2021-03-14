33ef381a0b3b0ba793d33cb0c5e8bdba1f488ab2
wayosu <wahyusman10@gmail.com>
2021-03-15 00:16:46 +0800

    pembaruan lokasi_post.php

--- views/page/lokasi/lokasi_post.php
+++ views/page/lokasi/lokasi_post.php
@@ -1,10 +1,21 @@
 <?php 
+
 include 'lokasi_function.php';
 include '../../../app/flash_message.php';
+include "../../../koneksi.php";
 	
 if ( isset($_POST['simpan_data'])) {
 	$nama_lokasi = $_POST['nama_lokasi'];
 	$link_lokasi = $_POST['share_lokasi'];
+
+	$query = "SELECT* FROM lokasi WHERE nama_lokasi='{$nama_lokasi}'";
+	$to_query = $mysqli->prepare($query);
+	$to_query->execute();
+	$result_query = $to_query->get_result();
+	if (mysqli_num_rows($result_query) > 0) {
+		flash("msg_gagal_data", "Nama lokasi yang anda masukkan sudah ada!");
+		return false;
+	}
 
 	simpan_data($nama_lokasi,$link_lokasi,$mysqli);
 	flash("msg_sukses_data", "Data berhasil di simpan");
